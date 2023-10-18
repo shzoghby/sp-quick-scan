1f806146baf3725bb23036cbf1ff232871b21b42
Sherief Elzoghby <szoghby@gmail.com>
2023-10-18 21:31:43 +1100

    initial

---
+++ AzureApp.ps1
@@ -0,0 +1,103 @@
+﻿#=====================================================================================================================
+# Script Name:             AzureApp.ps1
+# Description:             Azure app creation for Quick Archiving Scan for AvePoint
+# Author:                  Bruce Berends
+# Creation Date:           2023/10/13
+# Last Modified By:        Bruce Berends
+# Last Modified Date:      2023/10/13
+#=====================================================================================================================
+
+$outputCsv = "appdetails.csv"
+
+# Check if CSV exists and delete it if it does
+if (Test-Path $outputCsv) {
+    Remove-Item $outputCsv -Force
+}
+
+# 1. Install and import required modules
+if ($PSVersionTable.PSEdition -eq "Desktop" -and (Get-Module -Name AzureAD -ListAvailable)) {
+    Install-Module AzureAD -Force -AllowClobber -Scope CurrentUser
+    Import-Module AzureAD
+} else {
+    Install-Module AzureAD.Standard.Preview -Force -AllowClobber -Scope CurrentUser
+    Import-Module AzureAD.Standard.Preview
+}
+
+# 2. Login to Azure AD
+Connect-AzureAD
+
+# 3. Check if the application already exists
+$appName = "AvePointQuickScan"
+$app = Get-AzureADApplication -Filter "DisplayName eq '$appName'"
+
+
+
+if (-not $app) {
+# Create the Azure App without permissions first
+$app = New-AzureADApplication -DisplayName $appName
+$servicePrincipal = New-AzureADServicePrincipal -AppId $app.AppId
+
+if ($app -ne $null) {
+
+    $sp = Get-AzureADServicePrincipal -Filter "DisplayName eq '$appName'"
+        if (-not $sp) {
+            Write-Error "Service Principal for $appName not found."
+            exit
+         }
+
+    Start-Sleep -Seconds 10
+    # Retrieve the Service Principal for SharePoint Online
+    $spOnline = Get-AzureADServicePrincipal -Filter "AppId eq '00000003-0000-0ff1-ce00-000000000000'"
+
+    # Check if the permission already exists
+    $existingPermission = Get-AzureADServiceAppRoleAssignment -ObjectId $servicePrincipal.ObjectId | Where-Object {$_.ResourceId -eq $spOnline.ObjectId}
+
+    
+    # Grant Full Control permission to SharePoint Online if not already granted
+    if (-not $existingPermission) {
+    $fullControlPermission = $spOnline.AppRoles | Where-Object {$_.Value -eq "Sites.FullControl.All"}
+    New-AzureADServiceAppRoleAssignment -ObjectId $servicePrincipal.ObjectId -PrincipalId $servicePrincipal.ObjectId -Id $fullControlPermission.Id -ResourceId $spOnline.ObjectId
+    }
+
+    # Add the required permissions to the Azure AD app
+    #$servicePrincipal = Get-AzureADServicePrincipal -Filter "DisplayName eq '$appName'"
+    #New-AzureADServiceAppRoleAssignment -ObjectId $servicePrincipal.ObjectId -PrincipalId $servicePrincipal.ObjectId -ResourceId  (Get-AzureADServicePrincipal -Filter "AppId eq '00000003-0000-0ff1-ce00-000000000000'").ObjectId -Id (Get-AzureADServicePrincipal -Filter "AppId eq '00000003-0000-0ff1-ce00-000000000000'").AppRoles[0].Id
+ 
+         # Generate a certificate for the app
+        $certStartDate = (Get-Date).Date
+        $certEndDate = $certStartDate.AddYears(1)
+        $cert = New-SelfSignedCertificate -Subject "CN=$appName" -CertStoreLocation "Cert:\CurrentUser\My" -KeyExportPolicy Exportable -Provider "Microsoft Enhanced RSA and AES Cryptographic Provider" -NotAfter $certEndDate
+
+        # Convert the certificate into Base64 format
+        $base64Value = [System.Convert]::ToBase64String($cert.RawData)
+
+        # Construct the key credential
+        $certKeyCredential = New-Object Microsoft.Open.AzureAD.Model.KeyCredential
+        $certKeyCredential.CustomKeyIdentifier = [System.Convert]::FromBase64String([System.Convert]::ToBase64String($cert.GetCertHash()))
+        $certKeyCredential.EndDate = $certEndDate
+        $certKeyCredential.Value = [System.Text.Encoding]::Default.GetBytes($base64Value)
+        $certKeyCredential.StartDate = (Get-Date).AddMinutes(-10)
+        $certKeyCredential.Type = "AsymmetricX509Cert"
+        $certKeyCredential.Usage = "Verify"
+
+        # Add the key credential to the Azure AD app
+        Set-AzureADApplication -ObjectId $app.ObjectId -KeyCredentials @($certKeyCredential)
+
+        # Output to Console
+        Write-Output "App ID: $($app.AppId)"
+        Write-Output "Certificate Thumbprint: $($cert.Thumbprint)"
+
+        # Store the details for reference
+        $outputObject = [PSCustomObject]@{
+            'AppId'      = $app.AppId
+            'Thumbprint' = $cert.Thumbprint
+        }
+
+        $outputObject | Export-Csv -Path $outputCsv -NoTypeInformation -Append
+    } else {
+        # Handle the error appropriately
+        Write-Error "Azure AD application creation failed."
+    }
+} else {
+    Write-Warning "Azure AD application '$appName' already exists."
+}
---
+++ QuickScan.ps1
@@ -0,0 +1,103 @@
+#=====================================================================================================================
+# Script Name:             QuickScan.ps1
+# Description:             Quick Archiving Scan for AvePoint
+# Author:                  (Frank Ren) - Modified by Bruce Berends
+# Creation Date:           2023/10/13
+# Last Modified By:        Bruce Berends
+# Last Modified Date:      2023/10/13
+#=====================================================================================================================
+
+<#
+$csvData = Import-Csv -Path ".\AppDetails.csv"
+if (-not $csvData) {
+    Write-Error "AppDetails.csv not found. Please ensure it exists in the script directory."
+    exit
+}
+$appId = $csvData.AppId.Trim()
+$thumbprint = $csvData.Thumbprint.Trim()
+#>
+#========================================================
+#Variable Prompts
+#========================================================
+# Prompt for SharePoint credentials and Tenant information
+$Tenant = Read-Host -Prompt "Please enter your tenant name (e.g., yourdomain.onmicrosoft.com)"
+$TenantD = Read-Host -Prompt "Please enter your tenant Domain prefix (e.g., yourdomain)"
+
+
+#========================================================
+#Static Variables (no need to modify)
+#========================================================
+$pageSize=500
+$execPath = Split-Path $MyInvocation.MyCommand.Path -parent
+$jobId=[DateTime]::Now.ToString("yyyyMMddHHmmss")
+$jobFolder=Join-Path $execPath $jobId
+$outFileName =[System.String]::Format("Report_{0}.csv",[System.DateTime]::Now.ToString("yyyyMMddhhmmss"))
+$outFilePath = Join-Path $jobFolder $outFileName
+$inputFileName="sitecollection.csv"
+$inputFilePath=Join-Path $execPath $inputFileName
+$scopeSiteCollectionsFilePath=Join-Path $execPath 'scopeSIteCOllectionbs.csv'
+
+#========================================================
+#Functions module
+#=======================================================
+
+try
+{
+    . .\_functions.ps1
+}
+catch
+{
+    Write-Error "Could not load _functions.ps1 file. $_"
+    exit
+}
+
+#========================================================
+#Log module
+#=======================================================
+try
+{
+    $parent=Split-Path -Parent $MyInvocation.MyCommand.Definition
+    $logfile=[System.IO.Path]::Combine($parent,"Logs\Log.log")
+    $logpath=Split-Path -Parent $logfile
+    if (!(Test-Path $logpath)) {
+        New-Item -ItemType Directory -Force -Path $logpath
+    }
+}
+catch
+{
+    Write-Host "An error occured while writing errors to log. $_"
+}
+
+#========================================================
+#Assemblies Install
+#=======================================================
+Install-RequiredModules
+Import-Module PnP.PowerShell -ErrorAction SilentlyContinue # Ensure the PnP module is imported
+
+#========================================================
+#Retrieve SIte COllections
+#=======================================================
+Get-SiteCollections -tenant $TenantD
+
+#========================================================
+#Main Run
+#=======================================================
+try {
+     
+        $startTime=Get-Date
+        WriteInfoLog "The job is running on $($startTime)."
+        if([System.IO.Directory]::Exists($jobFolder) -eq $false){
+            [System.IO.Directory]::CreateDirectory($jobFolder) | Out-Null
+            WriteReport '"SiteCollection","Site","List","LastModified","TotalFileCount","TotalFileStreamSize","TotalSize","VersionSize"'
+        }
+        $sitecollections=Import-Csv -Path $inputFilePath 
+        foreach($sitecollection in $sitecollections){
+            WriteInfoLog "Begin scan the site collection $($sitecollection.URL)"
+            ScanSiteCollection $sitecollection.URL
+            WriteInfoLog "Finished scan the site collection $($sitecollection.URL)"
+        }
+        WriteInfoLog "The job is finished on $(Get-Date)."
+    }
+    catch {
+        WriteErrorLog "An error occurred while exporting the stroage of the excel.Exception:$($_)"   
+    }
